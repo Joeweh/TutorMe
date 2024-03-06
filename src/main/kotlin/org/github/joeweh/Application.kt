971b444bf1841f9704513f50a2934894714e70d7
Joeweh <joeyqsa@outlook.com>
2024-03-06 08:56:56 -0800

    Set port by env var instead of hard coding value

--- src/main/kotlin/org/github/joeweh/Application.kt
+++ src/main/kotlin/org/github/joeweh/Application.kt
@@ -6,7 +6,9 @@ import io.ktor.server.netty.*
 import org.github.joeweh.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    val port = Integer.parseInt(System.getenv("PORT"))
+
+    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
